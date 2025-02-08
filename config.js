90f2eb6f1c7b1e5fbc4b4201603523cda130f228
Monumodmod <163610394+Monumodmod@users.noreply.github.com>
2025-02-08 11:55:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "GMAX-MD~DchCWBQC#Ep2p4N5FkYNo_DkvkUNhXu3FHcL19ZX_6R1e_doGHvs",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
